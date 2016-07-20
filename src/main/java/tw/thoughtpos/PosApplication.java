70b8a87904e5fd88ed7740aa8d850cce07aca389
lijun003 <lijun359714823@163.com>
2016-07-20 16:56:33 +0800

    [M007][lijun] - fix bug

--- src/main/java/tw/thoughtpos/PosApplication.java
+++ src/main/java/tw/thoughtpos/PosApplication.java
@@ -1,13 +1,11 @@
 package tw.thoughtpos;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PosApplication {
-    @Autowired
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(PosApplication.class, args);
     }
 }
